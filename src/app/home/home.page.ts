8b2746ff651957ac68e8279c5f19adcfd550618e
bastok <bfromager@gmail.com>
2018-11-08 23:58:49 +0100

    Piece

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
+// https://stackoverflow.com/questions/8751020/how-to-get-a-pixels-x-y-coordinate-color-from-an-image
+// https://devdactic.com/ionic-canvas-drawing-files/
+
+// https://ionicframework.com/docs/api/components/content/Content/
+
 import { Component } from '@angular/core';
+import {Piece} from '../services/piece';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +13,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+    constructor() {
+        let a: number = 20;
+        let b: number = 50;
+        let c: boolean = true;
+        let d: boolean = false;
+        console.log(a && c, b && d, c && a, d && b);
+        let piece = new Piece([0,1,2,3,4,5,6,7,8]);
+        console.log("------------");
+        piece.setMask([0,0,0,0,0,0,0,0,8]);
+        console.log("------------");
+        piece.setMask([1,0,0,0,0,0,0,0,8]);
+        console.log("------------");
+        piece.setMask([1,0,0,0,1,0,0,0,8]);
+    }
 }
---
+++ src/app/services/piece.ts
@@ -0,0 +1,68 @@
+
+/*
+    0   1   2
+    7   8   3
+    6   5   4
+*/
+
+export type MaskArray = number[];
+
+export class Piece {
+    private _mask: MaskArray;
+    private _positions: MaskArray[];
+
+    constructor(mask?:MaskArray) {
+        if (mask) this.setMask(mask);
+    }
+
+    getPositions(): MaskArray[] {
+        return this._positions.slice();
+    }
+
+    setMask(mask : MaskArray)  {
+        this._positions = [];
+        this._mask = mask.slice();
+        this._positions.push(this._mask);
+        this.debugMask(this._mask);
+
+        for (let i=0; i<4; ++i) {
+            let newMask = this.rotateMask(this._mask,i);
+            if (this.isNewMask(newMask)) {
+                this._positions.push(newMask);
+                this.debugMask(newMask);
+            }
+        }
+    }
+
+    private debugMask(mask: MaskArray) {
+        console.log("");
+        console.log(mask[0] + " " + mask[1] + " " + mask[2]);
+        console.log(mask[7] + " " + mask[8] + " " + mask[3]);
+        console.log(mask[6] + " " + mask[5] + " " + mask[4]);
+    }
+
+    private rotateMask(mask : MaskArray, nbQuartDeTour: number): MaskArray {
+        let result: MaskArray = [];
+        result[mask.length-1] = mask[mask.length-1];
+        for (let i=0; i<8; ++i) {
+            let newIndex = (i + 2*nbQuartDeTour)%8;
+            result[newIndex] = mask[i];
+        }
+        return result;
+    }
+
+    private isEqualMask(fromMask : MaskArray, toMask : MaskArray): boolean {
+        for (let i in  fromMask) {
+            if (fromMask[i] != toMask[i]) return false;
+        }
+        return true;
+    }
+
+    private isNewMask(newMask : MaskArray): boolean {
+        for (let mask of this._positions) {
+            if (this.isEqualMask(newMask,mask))
+                return false;
+        }
+        return true;
+    }
+}
